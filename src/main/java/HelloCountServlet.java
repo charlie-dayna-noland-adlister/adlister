e241c767510e6e943b9837c409e629dd3799e12a
Noland Seigler <nolandseigler@gmail.com>
2020-02-26 11:30:28 -0600

    complete with bonuses

---
+++ src/main/java/HelloCountServlet.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Map;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet(name = "HelloCountServlet", urlPatterns = "/hello/count")
+public class HelloCountServlet extends HttpServlet {
+    int count;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        count++;
+        String name = request.getParameter("name");
+        String resetCount = request.getParameter("reset-count");
+        if (resetCount != null) {
+            count = 0;
+            response.getWriter().println("The count has been reset to 0");
+        }
+        if (name != null) {
+            response.getWriter().println(String.format("Hello, %s count is %d", name, count));
+        } else {
+            response.getWriter().println(String.format("Hello, count is %d", count));
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello!</h1>");
+    }
+}
---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Map;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        int count = 0;
+            String name = request.getParameter("name");
+            if (name != null) {
+                response.getWriter().println(String.format("Hello, %s", name));
+            } else {
+                response.getWriter().println("Hello, World!");
+            }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello!</h1>");
+    }
+}
