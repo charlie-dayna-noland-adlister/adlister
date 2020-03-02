42c55c5ec2e63b41978054c58d3a33fc5ca7327f
Noland Seigler <nolandseigler@gmail.com>
2020-03-02 11:52:08 -0600

    check point

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,23 +3,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null && session.getAttribute("user").equals("admin")) {
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
+        return;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
-
-        if (validAttempt) {
+        HttpSession session = request.getSession();
+        session.setAttribute("valid", validAttempt);
+        session.setAttribute("user", username);
+        if (validAttempt && session.getAttribute("user").equals("admin")) {
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+        return;
     }
 }
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -9,7 +9,8 @@
     <jsp:include page="partials/navbar.jsp" />
 
     <div class="container">
-        <h1>Viewing your profile.</h1>
+        <h1>Welcome <%= request.getSession().getAttribute("user") %></h1>
+        <h3>Viewing your profile.</h3>
     </div>
 
 </body>
